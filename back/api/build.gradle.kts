f44ccb88e42282d7a6120f741fb0f28c3b1c4eda
Andrey Belchenko <abelchenko@conteq.ru>
2025-03-10 03:37:25 +0300

    to sub proj

--- back/api/build.gradle.kts
+++ back/api/build.gradle.kts
@@ -1,13 +1,7 @@
-plugins {
-    id("org.jetbrains.kotlin.plugin.allopen") version "2.0.21"
-    id("io.quarkus")
-}
 
 dependencies {
-    implementation(enforcedPlatform("${property("quarkusPlatformGroupId")}:${property("quarkusPlatformArtifactId")}:${property("quarkusPlatformVersion")}"))
     implementation("io.quarkus:quarkus-rest-jackson")
     implementation("io.quarkus:quarkus-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-rest")
     implementation("io.quarkus:quarkus-smallrye-openapi")
@@ -16,9 +10,3 @@ dependencies {
     testImplementation("io.rest-assured:rest-assured")
 }
 
-allOpen {
-    annotation("jakarta.ws.rs.Path")
-    annotation("jakarta.enterprise.context.ApplicationScoped")
-    annotation("jakarta.persistence.Entity")
-    annotation("io.quarkus.test.junit.QuarkusTest")
-}
--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -1,26 +1,45 @@
 // Root build.gradle.kts
 plugins {
-    kotlin("jvm") version "2.0.21" apply false // Apply false to avoid applying it to the root project
+    kotlin("jvm") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.allopen") version "2.0.21" apply false
+    id("io.quarkus") apply false
 }
 
 subprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm") // Apply Kotlin plugin to all subprojects
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.allopen")
+    apply(plugin = "io.quarkus")
 
     repositories {
         mavenCentral()
         mavenLocal()
     }
 
-    // Configure Java and Kotlin for all subprojects
     configure<JavaPluginExtension> {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_17.toString()
-            javaParameters = true
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            javaParameters.set(true)
         }
+    }
+
+    // Add dependencies using the low-level API
+    dependencies {
+        add("implementation", enforcedPlatform("${property("quarkusPlatformGroupId")}:${property("quarkusPlatformArtifactId")}:${property("quarkusPlatformVersion")}"))
+        add("implementation", "org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    }
+
+    // Configure allOpen
+    configure<org.jetbrains.kotlin.allopen.gradle.AllOpenExtension> {
+        annotations(
+            "jakarta.ws.rs.Path",
+            "jakarta.enterprise.context.ApplicationScoped",
+            "jakarta.persistence.Entity",
+            "io.quarkus.test.junit.QuarkusTest"
+        )
     }
 }
